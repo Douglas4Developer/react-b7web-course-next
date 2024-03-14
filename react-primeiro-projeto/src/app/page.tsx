d1e94de297ab4a9f793eae5045bf1a5a3a2e6fd8
Douglas Soares de Souza Ferreira <105395813+Douglas4Developer@users.noreply.github.com>
2024-03-13 23:19:27 -0300

    Again Estudos

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Card } from "@/components/Card";
 import { Person } from "@/components/Person";
  
 
@@ -9,20 +10,14 @@ import { Person } from "@/components/Person";
       <h1 className="font-bold  text-2xl">Olá Mundo</h1> 
       <h3>Algum outro texto</h3>
         /* PROPS para dados dinamicos */
-       <Person 
-          name= "Elon Musk"
-          avatar= "https://s2-oglobo.glbimg.com/dtZTFBZPhkNzZQxcITGBiNbfzws=/0x0:4000x2668/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2023/0/r/6RAUoOSmSQDbSlBxnQoA/musk-sorrindo-bloomberg.jpg"
-          roles= {['Ceo da Tesla', 'Ceo da SpaceX']}
-       />
-       <Person 
-          name= "LaraCROFT"
-          avatar= "https://upload.wikimedia.org/wikipedia/pt/7/7a/Lara_Croft_Tomb_Raider.jpg"
-          roles= {['Ceo da Ubisodt', 'Ceo da caça de tesouros']}
-       />
-       <Person
-          name= "Douglas"
-          roles={['Algo', 'Dono de algo']}       
-       />
+
+        <Card>
+         <>
+            <h3 className="text-3xl font-bold italic"> Frase de efeito</h3>
+            <p className="text-right text-sm"> Autor desconhecido </p>
+         </>
+        </Card>
+   
 
     </div>
   
