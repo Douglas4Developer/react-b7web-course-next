0e8f40d016888e2ddc96941fe8961520744e6ea4
Douglas Soares de Souza Ferreira <105395813+Douglas4Developer@users.noreply.github.com>
2024-03-13 22:15:13 -0300

    Aprendizado 13-03-2024

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -14,7 +14,16 @@ import { Person } from "@/components/Person";
           avatar= "https://s2-oglobo.glbimg.com/dtZTFBZPhkNzZQxcITGBiNbfzws=/0x0:4000x2668/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2023/0/r/6RAUoOSmSQDbSlBxnQoA/musk-sorrindo-bloomberg.jpg"
           roles= {['Ceo da Tesla', 'Ceo da SpaceX']}
        />
- 
+       <Person 
+          name= "LaraCROFT"
+          avatar= "https://upload.wikimedia.org/wikipedia/pt/7/7a/Lara_Croft_Tomb_Raider.jpg"
+          roles= {['Ceo da Ubisodt', 'Ceo da caça de tesouros']}
+       />
+       <Person
+          name= "Douglas"
+          roles={['Algo', 'Dono de algo']}       
+       />
+
     </div>
   
    );
--- react-primeiro-projeto/src/components/Person.tsx
+++ react-primeiro-projeto/src/components/Person.tsx
@@ -3,39 +3,35 @@ const getWeekday=(today: Date)=>{
 }
 
 type Props = {
-    name: string;
-    avatar: string;
-    roles: string[];
-
-
+    name: string; //obrigatorio
+    avatar?: string;//obrigatorio
+    roles: string[]; // array de strings
+    address?: string; //obrigatorio opcional
 }
 
-export const Person = (props: Props)=>{
+export const Person = ({
+    name, 
+    avatar = 'https://static.vecteezy.com/ti/vetor-gratis/p1/9292244-default-avatar-icon-vector-of-social-media-user-vetor.jpg', 
+    roles
+}: Props)=>{
 // Variaveis posso criar variaveis // funcçoes e acessar no codigo ou criar const
-    const data = {
-        name: 'Elon Musk',
-        avatar: 'https://s2-oglobo.glbimg.com/dtZTFBZPhkNzZQxcITGBiNbfzws=/0x0:4000x2668/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2023/0/r/6RAUoOSmSQDbSlBxnQoA/musk-sorrindo-bloomberg.jpg',
-        roles: ['Ceo da Tesla', 'Ceo da SpaceX']
-    }
     
-    const today: Date = new Date();
-
     return(
-        <>
-           <h1 style={{color: 'red', fontSize: '30px'}}>Name: {data.name} - {getWeekday(today)}</h1>
+        <div className="p-1" >
+           <h1>{name}</h1>
            <img 
-            src={data.avatar} 
-            alt={data.name}
+            src={avatar} 
+            alt={name}
             className="w-40"
-        />          
+            />          
 
         <ul>
-            <li>{data.roles[0]}</li>
-            <li>{data.roles[1]}</li>
+            <li>{roles[0]}</li>
+            <li>{roles[1]}</li>
         
         </ul>
         
-        </>
+        </div>
     );
 
 }
